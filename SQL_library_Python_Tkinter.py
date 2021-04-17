df76ac68c6d679993a144ea40afeadd9be4684d3
WojciechMycek <66401382+WojciechMycek@users.noreply.github.com>
2021-04-17 15:33:30 +0200

    Added variables lists of books and created database

--- SQL_library_Python_Tkinter.py
+++ SQL_library_Python_Tkinter.py
@@ -2,14 +2,66 @@ from tkinter import *
 from PIL import ImageTk, Image
 import sqlite3
 
-class lists_books():
-    def __init__(self, tittle, author, published_house, is_it_borrow, who_get_it):
-        return 0
+root = Tk()
+root.title("SQL Library Python")
+root.geometry("1100x400")
 
-class users_library(lists_books):
-    def __init__(self,name, vorname, address, borrowed_books, user_id):
-        return 0
 
-class borrow_and_give_back(users_library,lists_books):
-    def __init__(self):
-        return 0
+#create a database
+
+conn = sqlite3.connect('Library_app.db')
+
+
+#create cursor
+c = conn.cursor()
+
+c.execute('''CREATE TABLE books(
+   tittle text,
+   author text,
+   who_get_it text,
+   publishing_house text,
+   date integer
+)''')
+
+def add():
+    return 0
+
+#date of books
+tittle = Entry(root, width=35)
+tittle.grid(row=0,column=1, padx=30,pady=(10,0))
+
+author = Entry(root, width=35)
+author.grid(row=1,column=1, padx=20)
+
+who_get_it = Entry(root, width=35)
+who_get_it.grid(row=2,column=1, padx=20)
+
+publishing_house = Entry(root, width=35)
+publishing_house.grid(row=3,column=1, padx=20)
+
+#print names of priting data
+date = Entry(root, width=35)
+date.grid(row=4,column=1, padx=20)
+
+tittle_label = Label(root,text="Tittle'Books")
+tittle_label.grid(row=0,column=0,pady=(10,0))
+
+author_label = Label(root,text="Author's Book")
+author_label.grid(row=1,column=0)
+
+who_get_it_label = Label(root,text="Who get it")
+who_get_it_label.grid(row=2,column=0)
+
+
+publishing_label = Label(root,text="Published by")
+publishing_label.grid(row=3,column=0)
+
+date_label = Label(root,text="Date")
+date_label.grid(row=4,column=0)
+
+# Create Add Button
+submit_btn = Button(root, text="Add record to Database", command=add)
+submit_btn.grid(row=5,column=0,columnspan=2,pady=10,padx=10,ipadx=100)
+
+
+root.mainloop()
